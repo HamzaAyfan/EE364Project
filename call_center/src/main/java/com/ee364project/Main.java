7dc2302ddb803c602867c0dc7c4e07e7a21276f5
MshariAsiri <109319432+MshariAsiri@users.noreply.github.com>
2023-11-28 20:58:26 +0300

    Csv path edit test

--- call_center/src/main/java/com/ee364project/Main.java
+++ call_center/src/main/java/com/ee364project/Main.java
@@ -4,7 +4,7 @@ import com.ee364project.file_manage.Csv;
 
 public class Main {
     public static void main(String[] args) {
-        HasData[] data = Csv.read("C:\\Users\\fesal\\Desktop\\EE364 GitHub\\EE364Project\\call_center\\input\\Problem.csv");
+        HasData[] data = Csv.read("C:\\Users\\Mshar\\Desktop\\GitHub Rep\\EE364Project\\call_center\\input\\Problem.csv");
         for (HasData datum : data) {
             System.out.println(datum);
         }
